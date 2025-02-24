e7ea20e45af688f0019ba90fd908ae6ad41d94f0
barbara.lamazales <moraeslamazales@gmail.com>
2025-02-23 22:42:55 -0300

    junção de tabelas

--- exercicio_sql.py
+++ exercicio_sql.py
@@ -91,6 +91,38 @@ cursor = conexao.cursor()
 #b) Remova um cliente pelo seu ID
 #cursor.execute('DELETE FROM clientes WHERE id=4')
 
+#8. Junção de Tabelas
+#Crie uma segunda tabela chamada "compras" com os campos: id (chave primária), 
+#cliente_id (chave estrangeira referenciando o id da tabela "clientes"), produto (texto) 
+#e valor (real). Insira algumas compras associadas a clientes existentes na tabela 
+#"clientes". Escreva uma consulta para exibir o nome do cliente, o produto e o
+#valor de cada compra.
+
+#cursor.execute('''CREATE TABLE compras(id INT PRIMARY KEY,
+ #              cliente_id INT,
+  #             produto VARCHAR(100), 
+   #            valor float(100), 
+    #           FOREIGN KEY (cliente_id) REFERENCES clientes(id));''')
+
+#cursor.execute('INSERT INTO compras(id,produto, valor,cliente_id)VALUES(1,"Panela",50,2)')
+#cursor.execute('INSERT INTO compras(id,produto, valor,cliente_id)VALUES(2,"Celular",1500,3)')
+#cursor.execute('INSERT INTO compras(id,produto, valor,cliente_id)VALUES(3,"DVD",100,2)')
+#cursor.execute('INSERT INTO compras(id,produto, valor,cliente_id)VALUES(5,"Ventilador",100,5)')
+#cursor.execute('INSERT INTO compras(id,produto, valor,cliente_id)VALUES(4,"Tênis",250,6)')
+
+#dados = cursor.execute('SELECT * FROM clientes INNER JOIN compras ON clientes.id = compras.cliente_id')
+#for usuario in dados:
+#    print(usuario)  
+
+
+dados = cursor.execute('''
+    SELECT clientes.nome, compras.produto, compras.valor
+    FROM clientes
+    INNER JOIN compras ON clientes.id = compras.cliente_id
+''')
+
+for usuario in dados:
+    print(usuario)
 
 
 
