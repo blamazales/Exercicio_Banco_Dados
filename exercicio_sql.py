92fbfa8c63ba5daae195f99c9cb48ab6952595d1
barbara.lamazales <moraeslamazales@gmail.com>
2025-02-23 21:11:43 -0300

    atualizacao de registros, criação de nova tabela e inserção de dados na nova tabela

--- exercicio_sql.py
+++ exercicio_sql.py
@@ -42,10 +42,27 @@ cursor = conexao.cursor()
 
 #4. Atualização e Remoção
 #a) Atualize a idade de um aluno específico na tabela.
+#cursor.execute('UPDATE alunos SET nome="José" WHERE nome="Jose"')  
+
+
 #b) Remova um aluno pelo seu ID
+#cursor.execute('DELETE FROM alunos WHERE id=4')
 
+#5. Criar uma Tabela e Inserir Dados
+#Crie uma tabela chamada "clientes" com os campos: id (chave
+#primária), nome (texto), idade (inteiro) e saldo (float). Insira alguns
+#registros de clientes na tabela.
 
+#cursor.execute('CREATE TABLE clientes(id INT PRIMARY KEY, nome VARCHAR(100), idade int(100), saldo FLOAT);')
+#cursor.execute('INSERT INTO clientes(id,nome,idade,saldo)VALUES(2,"Maria",50,2000.50)')
+#cursor.execute('INSERT INTO clientes(id,nome,idade,saldo)VALUES(3,"João",35,5500)')
+#cursor.execute('INSERT INTO clientes(id,nome,idade,saldo)VALUES(4,"Alessandra",23,3000)')
+#cursor.execute('INSERT INTO clientes(id,nome,idade,saldo)VALUES(5,"David",30,3350.50)')
+#cursor.execute('INSERT INTO clientes(id,nome,idade,saldo)VALUES(6,"Teo",19,350.50)')
 
+#6. Consultas e Funções Agregadas
+#Escreva consultas SQL para realizar as seguintes tarefas:
+#a) Selecione o nome e a idade dos clientes com idade superior a 30 anos.
 
 
 
